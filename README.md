4d83e87b2cb8d25cf6d8c17fc2c2212a30040063
Bikram Phurumbo <bikramphurumbo@Mangnewas-MacBook-Pro-3.local>
2024-08-30 18:32:51 -0400

    objects

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # JavaScript-Basics
+skibidi dop dop dop dop
---
+++ index.js
@@ -0,0 +1,26 @@
+// my first JS code!
+console.log('Hello World');
+
+
+//variable 
+let sigma = 'sigma';
+console.log(sigma);
+
+//constants
+let interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate);
+
+//primitive types
+let name = 'mango'; // string literal
+let age = '30'; //number literal
+let isApproved = false; // boolean literal
+let firstName = undefined; 
+let selectedColor = null;
+
+//objects
+let person = {
+    name: 'mualani',
+    age: '30'
+};
+console.log(person);
